1684310b0179a1ea033be6f45af24300a0da7944
romikvers <koshivr032@gmail.com>
2021-07-26 10:43:46 +0300

    26.07.21

---
+++ Getreide.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include "windows.h"
+
+using namespace std;
+
+void Gleich1();
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	
+	Gleich1();
+	
+	return 0;
+}
+void Gleich1()
+{
+	int i;
+	long long S = 1;
+	int P = 0;
+	int zahl = 2;
+	for (i = 2; i <= 64; i++)
+	{
+		P = zahl * zahl;
+		S += P;
+		if (S >= 2000000)
+		{
+			cout << "reshitok: " << i << " for 1000 zeron" << endl;
+			break;
+		}
+		zahl = P;
+	}
+	cout << "\nS :" << S << endl;
+	cout << "i: " <<  i;
+}
---
+++ vector_string.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <conio.h>
+#include <stdlib.h>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	vector<string> names;
+	vector<int> years;
+	string name_priz;
+	int year;
+	char ch;
+	int i = 0;
+	int gesetz = 0;
+	int siz_nam = size(names);
+	do {
+		cout << "Character input name and year :"; cin >> name_priz >> year;
+		for (i = 0; i < siz_nam; i++)
+		{
+			if (name_priz == names[i] && year == years[i])
+			{	
+				gesetz = 1;
+				cout << "Data already is in file" << endl;
+				break;
+			}
+		}
+		if (gesetz == 0)
+		{
+			names.push_back(name_priz);
+			years.push_back(year);
+		}
+
+		cout << "Continue(Y/N y/n)? :"; cin >> ch;
+	} while (ch == 'Y' || ch == 'y');
+	int siz_nams = size(names);
+	cin.clear(); // очищаем ввод
+	cin.good(); // оповещаем что все ок
+	for (i = 0; i < siz_nams; i++)
+	{
+		cout << "names :" << names[i] << endl;
+		cout << "years :" << years[i] << endl;
+	}
+	cout << "\nreturn\n" << endl;
+	return 0;
+}
